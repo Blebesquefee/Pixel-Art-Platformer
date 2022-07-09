b5c155ed2f24bfc23aaf8f63e25d93236e35d28d
Blebesquefee <arthur.gosset@epita.fr>
2022-07-09 20:58:49 +0200

    Slime/Bat/Mushroom done

--- Space_Pixel_Art_Platformer/Assets/Scripts/Enemy_Health.cs
+++ Space_Pixel_Art_Platformer/Assets/Scripts/Enemy_Health.cs
@@ -11,20 +11,23 @@ public class Enemy_Health : MonoBehaviour
     public int health;
     public GameObject enemy;
     public Animator animator;
+    public float delay;
 
     public void TakeDamage(int value)
     {
         health -= value;
         if (health < 0)
         {
-            //animator.SetBool("death", true);
+            animator.SetBool("death", true);
             StartCoroutine(Death());
         }
+        else
+            animator.Play("hit");
     }
 
     IEnumerator Death()
     {
-        yield return new WaitForSecondsRealtime(2);
+        yield return new WaitForSecondsRealtime(delay);
         Destroy(enemy);
     }
 }
--- Space_Pixel_Art_Platformer/Assets/Scripts/Enemy_Weakspot.cs
+++ Space_Pixel_Art_Platformer/Assets/Scripts/Enemy_Weakspot.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Enemy_Weakspot : MonoBehaviour
 {
     // Public Field
     public GameObject toDestroy;
+    public Animator animator;
+    public float delay;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !collision.CompareTag("PlayerAttack"))
-            Destroy(toDestroy);
+        {
+            animator.SetBool("death", true);
+            StartCoroutine(Death());
+        }
+    }
+
+
+    IEnumerator Death()
+    {
+        yield return new WaitForSecondsRealtime(delay);
+        Destroy(toDestroy);
     }
 }
